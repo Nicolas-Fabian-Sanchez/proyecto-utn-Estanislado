54bace5d1207d58e32120afd88ceec073d3d6edf
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2023-03-24 21:29:43 -0300

    primer commit

---
+++ src/Home/home.js
@@ -0,0 +1,21 @@
+import './home.css';
+
+export default function Home(){
+  return(
+      <header>
+           <h1>"Bienvenidos a Estanislao"</h1>
+           <form>
+              <legend>Por favor ingrese el numero de mesa donde se encuentra sentado</legend>
+              <div>
+                <label><input type="radio"></input>Mesa 1</label>
+                <label><input type="radio"></input>Mesa 2</label>
+                <label><input type="radio"></input>Mesa 3</label>
+                <label><input type="radio"></input>Mesa 4</label>
+              </div>
+              <button type="button">Ingresar</button>
+           </form>
+         
+      </header>
+  )
+   
+}
